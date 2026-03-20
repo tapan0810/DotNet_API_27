c4479005553ee7de06a71209cff634b527e15824
tapan0810 <arceus0810@gmail.com>
2026-03-20 21:36:46 +0530

    feat : Implemented Class Model

---
+++ DotNet_API_27/Entities/Models/Class.cs
@@ -0,0 +1,11 @@
+﻿namespace DotNet_API_27.Entities.Models
+{
+    public class Class
+    {
+        public int Id { get; set; }
+        public string ClassName { get; set; } = string.Empty;
+        public char Section {  get; set; } = 'A';
+        public bool IsClass { get; set; } = false;
+
+    }
+}

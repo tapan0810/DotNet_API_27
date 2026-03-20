0bd9a70209ce054112eb31d889b072fa5a691779
tapan0810 <arceus0810@gmail.com>
2026-03-20 21:40:12 +0530

    feat : Implemented Class DTOs

---
+++ DotNet_API_27/Entities/DTOs/ClassDTO/GetClassByIdDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNet_API_27.Entities.DTOs.ClassDTO
+{
+    public class GetClassByIdDTO
+    {
+        public int Id { get; set; }
+        public string ClassName { get; set; } = string.Empty;
+        public char Section { get; set; } = 'A';
+        public bool IsClass { get; set; } = false;
+    }
+}
---
+++ DotNet_API_27/Entities/DTOs/ClassDTO/UpdateClassDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNet_API_27.Entities.DTOs.ClassDTO
+{
+    public class UpdateClassDTO
+    {
+        public string ClassName { get; set; } = string.Empty;
+        public char Section { get; set; } = 'A';
+        public bool IsClass { get; set; } = false;
+    }
+}

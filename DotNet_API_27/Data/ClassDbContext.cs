68f8e2cda244e631742bed81ec453cc91897c2e1
tapan0810 <arceus0810@gmail.com>
2026-03-20 21:43:37 +0530

    Feat : Implemented ClassDbContext

---
+++ DotNet_API_27/Data/ClassDbContext.cs
@@ -0,0 +1,12 @@
+﻿using DotNet_API_27.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet_API_27.Data
+{
+    public class ClassDbContext:DbContext
+    {
+        public ClassDbContext(DbContextOptions<ClassDbContext> options) : base(options) { }
+
+        public DbSet<Class> Classes => Set<Class>();
+    }
+}

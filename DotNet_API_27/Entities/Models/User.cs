57dcf08553da0043cfd236179d64b7ef65a4989a
tapan0810 <arceus0810@gmail.com>
2026-03-24 23:53:23 +0530

    feat : Implemented User Model

---
+++ DotNet_API_27/Entities/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNet_API_27.Entities.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+
+        public string HashPassword { get; set; } = string.Empty;
+    }
+}

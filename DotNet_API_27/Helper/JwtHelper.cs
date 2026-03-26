0ec6a72602b6b0da1d699367ab81048caff26d31
tapan0810 <arceus0810@gmail.com>
2026-03-26 23:40:57 +0530

    feat : Implementing JwtHelper

---
+++ DotNet_API_27/Helper/JwtHelper.cs
@@ -0,0 +1,23 @@
+﻿using DotNet_API_27.Entities.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+
+namespace DotNet_API_27.Helper
+{
+    public class JwtHelper(IConfiguration configuration) : IJwtHelper
+    {
+        public string GenerateToken(User user)
+        {
+            var claim = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,user.UserName),
+                new Claim(ClaimTypes.NameIdentifier,user.Id.ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["AppSettings:"]!));
+
+            var creds = 
+        }
+    }
+}
